4b6381d89c48517daf9bc886b8afb4f729fb12eb
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2019-09-06 15:03:20 +0700

    add login activity

---
+++ app/src/main/java/com/simx/kittycat/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.simx.kittycat.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simx.kittycat.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
